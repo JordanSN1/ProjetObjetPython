f6c9be346a3294ef43bde4820940f4cebce25c07
jordanSN1 <jordan.turnaco@ecoles-epsi.net>
2024-02-15 16:06:37 +0100

    modification du nom de la fonction et attribut

--- Password.py
+++ Password.py
@@ -1,27 +1,31 @@
 import string as st
 import random
+
+
 class Password:
-    def __init__(self, password = ""):
+    def __init__(self, password=""):
         self.password = password
         self.caracteres = [st.punctuation, st.ascii_letters, st.digits]
+        self.taille = 0
 
-    def GenererMDP(self,taille = 0):
-        while True :
-            taille = int(input("Veuillez saisir la taille pour votre mot de passe (Min : 12 , Max : 45): "))
-            if taille < 12:
+    def generer_mdp(self):
+        while True:
+            self.taille = int(input("Veuillez saisir la taille pour votre mot de passe (Min : 12 , Max : 25): "))
+            if self.taille < 12:
                 print("Le mot de passe doit contenir au moins 12 caractères")
 
-            elif taille > 45:
-                print("Le mot de passe doit contenir au plus 45 caractères")
+            elif self.taille > 25:
+                print("Le mot de passe doit contenir au plus 25 caractères")
             else:
                 break
         while True:
 
-            for i in range(taille):
-                index = random.randint(0,2)
-                self.password += self.caracteres[index][random.randint(0,len(self.caracteres[index]))-1]
+            for i in range(self.taille):
+                index = random.randint(0, 2)
+                self.password += self.caracteres[index][random.randint(0, len(self.caracteres[index])) - 1]
 
             return self.password
 
+
 nabil = Password()
-print(nabil.GenererMDP())
+print(nabil.generer_mdp())
