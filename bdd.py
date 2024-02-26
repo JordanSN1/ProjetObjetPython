51f8f52d36d248400d7f2ea1db7a0d0027c44cb2
jordanSN1 <jordan.turnaco@ecoles-epsi.net>
2024-02-26 16:33:47 +0100

    del des fichiers inutile

--- bdd.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-import config
-
-connection = mysql.connector.connect(user=config.DBConfig['user'], password=config.DBConfig['password'],
-                                     host=config.DBConfig['host'], database='sntlabo')
-
-cursor = connection.cursor()
